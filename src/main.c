4be1d2a519dc5384a18bf1be396aec5316c501cf
MarcelooGB <80929592+MarcelooGB@users.noreply.github.com>
2024-11-07 23:53:44 -0300

    prints de debug de ambos dispositivos

--- src/main.c
+++ src/main.c
@@ -19,14 +19,16 @@ static const char*  TAG = "2024CisternaUTRs";
 
 
 #define PinCaudalimetro 35
-#define PinTrigger 0
-#define PinEcho 4 
+#define PinTrigger 32
+#define PinEcho 33
 #define DistanciaMaximaCM 500 // 450 + ~10%
 
 //Declaracion de variables globales 
-long caudalContador=0; 
+long caudalContador=1; 
 float caudaltemporal=0;
 float caudalHora=0; //Indica la cantidad de litros que salieron del tanque 
+static long indiceSegundo = 0;
+uint64_t pulsosPorSegundo[3600] = {0}; // Asignación dinámica
 
 volatile uint64_t  tiempoInicio=0;
 volatile uint64_t  tiempoFin=0;
@@ -35,76 +37,15 @@ float distancia=0; // indica la cantida de litros dentro del tanque. Nivel.
 uint64_t duracion=0;
 float timeout = 0; //microsegundos
 
-/*
-#define PinMotor 23  //DELETE
-#define botonISR 27 //asigmo el pin 27 para recibir una interrupción externa.   DELETE
-static int icontISR = 0; // contador de veces que se ejecutó la interrupción.   DELETE
-static TickType_t ultimaISR; //tiempo en que se ejecutó por última vez la interrupción  DELETE 
-
-
-//función que ejecuta la rutina de interrrupción
-void ISRAlarma(void *args) //DELETE
-{
- // Controlo que hayan pasado al menos 20 milisengudo entre cada interrupción. 
- // ya que el switch de final de carrera no tiene demasiada sensibilidad y produce varias ISR simultaneas.
- if (xTaskGetTickCountFromISR() > (ultimaISR + 20))
- {
-    ultimaISR = xTaskGetTickCountFromISR() ;
-    icontISR++; 
- };
-
-}
-
-void InitISR()  //DELETE
-{
-    //Configuramos las propiedades de GPIO donde conectaremos la interrupción
-    gpio_config_t gpioISR;
-    gpioISR.pin_bit_mask = (1ULL << botonISR); 
-    gpioISR.mode  = GPIO_MODE_DEF_INPUT;
-    gpioISR.pull_up_en = GPIO_PULLUP_DISABLE;
-    gpioISR.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    gpioISR.intr_type = GPIO_INTR_POSEDGE;
-    gpio_config(&gpioISR);
-
-    //llamo a iniciar el servicio de interrupciones en Freertos 
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(botonISR, ISRAlarma, NULL);
-
-//Nota: generar dos estructuras 
-}
- 
-
-//TaskHandle_t handleATarea1; //aquí recibo el manejador para la tarea una vez creada   DELETE
-
-//función que ejecutará la Tarea 1 
-void Tarea1(void *param) //DELETE
-{
-    unsigned long ulContador=0;
-
-    for (;;)
-    {
-        ulContador++;
-        printf("Estoy dentro de la tarea 1. Vuelta [%lu] y ejecuta en el nucleo [%lu]\n", ulContador, (unsigned long) xTaskGetCoreID(handleATarea1) ); 
-        for ( long l = 0; l < 1000000; l++)
-        {
-            xTaskGetCoreID(handleATarea1);
-        }
-    
-
-        vTaskDelay(30/ portTICK_PERIOD_MS); 
-    }
-}
-*/
-
 void ISRCaudalimetro(void *args)
 {
     caudalContador++;
+    //printf("CaudalContador =[%li]\n", caudalContador);
 }
 
 void CalcularCaudal(void *args)
 {
-        static long indiceSegundo = 0;
-        float pulsosPorSegundo[3600] = {0};
+
         float caudalAuxiliar = 0;    
 
         while(1){
@@ -116,7 +57,7 @@ void CalcularCaudal(void *args)
         pulsosPorSegundo[indiceSegundo] = caudaltemporal;  //sumatoria de L en la ultima hora
 
         indiceSegundo = (indiceSegundo + 1) % 3600;  // buffer temporal
-
+        //printf("indiceSegundo =[%li]\n", indiceSegundo);
 
         for (int i = 0 ; i < 3600 ; i++){
             caudalAuxiliar += pulsosPorSegundo[i];  //auxiliar para evitar que caudalhora alguna vez sea 0
@@ -126,12 +67,15 @@ void CalcularCaudal(void *args)
         
         caudalAuxiliar = 0;
 
-        vTaskDelay(1000/ portTICK_PERIOD_MS); 
+        //printf("Caudal =[%f]\n", caudalHora);
+
+        vTaskDelay(2000/ portTICK_PERIOD_MS); 
     }
 }
 
 void IRAM_ATTR ISREcho(void *args) //IRAM_ATTR lo ejecuta en RAM en vez de ROM
 {
+    printf("3\n");
     if (gpio_get_level(PinEcho)) { // Flanco ascendente
         tiempoInicio = esp_timer_get_time(); // Guarda el tiempo de inicio
 
@@ -142,6 +86,7 @@ void IRAM_ATTR ISREcho(void *args) //IRAM_ATTR lo ejecuta en RAM en vez de ROM
         if (tiempoFin > tiempoInicio) {
             duracion = tiempoFin - tiempoInicio; // Duración del pulso en microsegundos
             distancia = (duracion / 1000000) * 343000 / 2; // Distancia (milimetros) = duracion en segundos * 343000mm/s /2
+            printf("duracion=[%li]  Distancia=[%f]\n", caudalContador,distancia);
         }
     }
 }
@@ -157,19 +102,22 @@ void CalculoDistancia(void *args)
         if(!gpio_get_level(PinEcho)){
 
             gpio_set_level(PinTrigger, 1); // Activa el Trigger
-            esp_rom_delay_us(10); // Mantiene el Trigger activo por al menos 10 microsegundos
+            esp_rom_delay_us(15); // Mantiene el Trigger activo por al menos 10 microsegundos
             gpio_set_level(PinTrigger, 0); // Desactiva el Trigger
 
+            printf("Trigger: %d. Echo: %d\n", gpio_get_level(PinTrigger), gpio_get_level(PinEcho));
             
             tiempoLimite = esp_timer_get_time() + timeout; // Timeout en microsegundos
 
             // Espera a que el Echo regrese o a que se alcance el tiempo máximo
             while (gpio_get_level(PinEcho) && esp_timer_get_time() < tiempoLimite) {
+                printf("Tiempo Limite =[%f]. Yo atrapado en while\n", tiempoLimite);
                 // Espera hasta que el Echo vuelva a 0 o se haya alcanzado el timeout
             }
 
             if (esp_timer_get_time() >= tiempoLimite) {
-                distancia = -1; // Indicamos que no hay medida disponible
+                distancia = -1; // Resolver que hacer si tenemos un timeout
+                printf("Tiempo Limite sobrepasado =[%f]\n", tiempoLimite);
             }
 
         }
@@ -185,11 +133,10 @@ void InitCaudalimetroISR()
     gpioCaudalimetro.pin_bit_mask = (1ULL << PinCaudalimetro); 
     gpioCaudalimetro.mode = GPIO_MODE_INPUT;
     gpioCaudalimetro.pull_up_en = GPIO_PULLUP_ENABLE;
-    gpioCaudalimetro.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    gpioCaudalimetro.intr_type = GPIO_INTR_NEGEDGE; // Detecta flanco de bajada
+    gpioCaudalimetro.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    gpioCaudalimetro.intr_type = GPIO_INTR_ANYEDGE; // Detecta flanco de subida. y de bajada
     gpio_config(&gpioCaudalimetro);
 
-    gpio_install_isr_service(0); 
     gpio_isr_handler_add(PinCaudalimetro, ISRCaudalimetro, NULL);
 }
  
@@ -206,37 +153,24 @@ void InitUltrasonico() {
     gpio_config_t echoConfig;
     echoConfig.pin_bit_mask = (1ULL << PinEcho);
     echoConfig.mode = GPIO_MODE_INPUT;
-    echoConfig.pull_up_en = GPIO_PULLUP_DISABLE;
+    echoConfig.pull_up_en = GPIO_PULLUP_ENABLE;
     echoConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
     echoConfig.intr_type = GPIO_INTR_ANYEDGE;
     gpio_config(&echoConfig);
 
-    gpio_install_isr_service(0);
+
     gpio_isr_handler_add(PinEcho, ISREcho, NULL);
 }
 
 void app_main() 
 {
-    /*
-    //creo 1 tarea 
-    //xTaskCreate(Tarea1, "Tarea 1", 2048, NULL,1, &handleATarea1 );
-    // Creo la tarea 1 y la asigno al segundo nucleo
-    //xTaskCreatePinnedToCore(Tarea1, "Tarea 1", 2048, NULL,1, &handleATarea1,0);
-
-
-    //Creamos una interrupción externa asociada al puerto 27. 
-    InitISR(); //DELETE
-
-    //Iniclizamos las propiedades del GPIO 23 
-    gpio_reset_pin(PinMotor); //DELETE
-    gpio_set_direction(PinMotor, GPIO_MODE_DEF_OUTPUT); //DELETE 
-    */
-
+    gpio_install_isr_service(0);
     InitCaudalimetroISR();
     InitUltrasonico();
 
+
     gpio_reset_pin(PinCaudalimetro);
-    gpio_set_direction(PinCaudalimetro, GPIO_MODE_DEF_OUTPUT); 
+    gpio_set_direction(PinCaudalimetro, GPIO_MODE_INPUT); 
 
     gpio_reset_pin(PinTrigger);
     gpio_set_direction(PinTrigger, GPIO_MODE_DEF_OUTPUT); 
@@ -247,36 +181,12 @@ void app_main()
     xTaskCreate(CalculoDistancia, "TareaUltrasonido", 2048, NULL, 1, NULL);
 
 
-    //bool bandera = true; //DELETE
-
-
-    while (1)
+    /*while (1)
     {
-        // aqui genero la señal de eco para luego recibir la interrupcion 
-
-    
-        /*if (bandera){ //DELETE
-        bandera = !bandera;
-        gpio_set_level(PinMotor, 0);
-
-        }else{
-        bandera = !bandera;
-        gpio_set_level(PinMotor, 1);
-
-        
-        senalgenerada = ahora(); 
-        ecorecibido = ahora(); 
-        duracionEco = ecorecibido  - senalgenerada; 
-        altura = ...   
-        
-        }
-    
-
-        //printf("Ejecutando el programa principaL ejecuta nucleo [%lu]\n", (unsigned long) xTaskGetCoreID(xTaskGetCurrentTaskHandle()));
-        */
 
         printf("Caudal =[%f], Distancia=[%f]\n", caudalHora, distancia);
         vTaskDelay(1000/ portTICK_PERIOD_MS);
 
     }
+    */
 }
