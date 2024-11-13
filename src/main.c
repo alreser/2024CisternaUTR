756c695ef5077d89873058d9ec5e3af7bb6e8859
Amadeo López Reser <alreser@hotmail.com>
2024-11-13 18:06:05 -0300

    añadido envío de valores por UART. CaudalHora y Nivel

--- src/main.c
+++ src/main.c
@@ -9,10 +9,14 @@ SoC: ESP32
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <stdio.h>
+#include <driver/uart.h>
 #include <driver/gpio.h>
 #include <esp_log.h>
 #include <esp_timer.h>
 #include <esp_system.h>
+#include <string.h>
+#include <esp_log.h>
+
 
 //Declaración de variables globales e inicializar funciones
 static const char*  TAG = "2024CisternaUTRs";
@@ -23,6 +27,9 @@ static const char*  TAG = "2024CisternaUTRs";
 #define PinTrigger 32
 #define PinEcho 33
 #define DistanciaMaximaCM 900 // 800 + ~10%
+#define UART_Numero UART_NUM_0   // Deberian ser los pines 1 y 3. 
+#define UART_TiempoEnvioMS 500 // frecuencia de envio de datos en milisegundos
+ 
 
 //Declaracion de variables globales 
 long caudalContador=0; 
@@ -104,8 +111,9 @@ void CalculoDistancia(void *args)
     
     while (1)
     {    
-        printf("EMPIEZA CICLO. Trigger: %d. Echo: %d\n", gpio_get_level(PinTrigger), gpio_get_level(PinEcho));
-        printf("Tiempo Inicio =[%"PRIu64"]. Tiempo Fin =[%"PRIu64"]. Duracion =[%"PRIu64"]\n", tiempoInicio, tiempoFin, duracion);
+        //ESP_LOGW(TAG,"EMPIEZA CICLO. Trigger: %d. Echo: %d\n", gpio_get_level(PinTrigger), gpio_get_level(PinEcho));
+        //printf("EMPIEZA CICLO. Trigger: %d. Echo: %d\n", gpio_get_level(PinTrigger), gpio_get_level(PinEcho));
+        //printf("Tiempo Inicio =[%"PRIu64"]. Tiempo Fin =[%"PRIu64"]. Duracion =[%"PRIu64"]\n", tiempoInicio, tiempoFin, duracion);
 
 
         if(gpio_get_level(!PinEcho)){
@@ -116,26 +124,26 @@ void CalculoDistancia(void *args)
             esp_rom_delay_us(10); // Mantiene el Trigger activo por al menos 10 microsegundos
             gpio_set_level(PinTrigger, 0); // Desactiva el Trigger
 
-            printf("Trigger: %d. Echo: %d\n", gpio_get_level(PinTrigger), gpio_get_level(PinEcho));
+           // printf("Trigger: %d. Echo: %d\n", gpio_get_level(PinTrigger), gpio_get_level(PinEcho));
             
             
             tiempoLimite = esp_timer_get_time() + timeout; // Timeout en microsegundos
 
             // Espera a que el Echo regrese o a que se alcance el tiempo máximo
             while (gpio_get_level(PinEcho) && esp_timer_get_time() < tiempoLimite) {
-                printf("Tiempo Limite =[%f]. Esperando al Echo o al Timeout\n", tiempoLimite);
+                //printf("Tiempo Limite =[%f]. Esperando al Echo o al Timeout\n", tiempoLimite);
                 // Espera hasta que el Echo vuelva a 0 o se haya alcanzado el timeout
             }
 
             if (esp_timer_get_time() >= tiempoLimite) {
                 distancia = -1; // Resolver que hacer si tenemos un timeout
-                printf("Tiempo Limite sobrepasado =[%f]\n", tiempoLimite);
+                //printf("Tiempo Limite sobrepasado =[%f]\n", tiempoLimite);
             }
             
         }
 
         vTaskDelay(1000/ portTICK_PERIOD_MS); // Espera un segundo antes de la siguiente medición
-        printf("Distancia=[%f]\n", distancia);
+        //printf("Distancia=[%f]\n", distancia);
 
     }
 }
@@ -166,6 +174,54 @@ void InitUltrasonico() {
     gpio_isr_handler_add(PinEcho, ISREcho, NULL);
 }
 
+/// @brief funcion de inicialización de los parámetros de Universal Asynchronous Receiver and Trasnmitter
+void InitUART(){
+
+ //inicializo el puerto UART   
+ uart_config_t uart_configt = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(UART_Numero, &uart_configt);
+    uart_set_pin(UART_Numero, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE); // TX: 17, RX: 16
+    uart_driver_install(UART_Numero, 1024 * 2, 0, 0, NULL, 0);
+
+
+
+}
+
+void UART_EnviarLecturas(void *args){
+
+
+    //Envio los valores sensados y estados del RTU 
+    char snivel[30]; 
+    char scaudalH[30];
+
+    while (1)
+    {
+        /* code */
+    
+    //Envio el Nivel    
+    sprintf(snivel,"VAL_NIVEL=%.0f", distancia );
+    uart_write_bytes(UART_Numero, snivel, strlen(snivel));
+    uart_write_bytes(UART_Numero, "\n", 1); // Agregar salto de línea para separar los comandos
+    
+
+        //Envio el Caudal    
+    sprintf(scaudalH,"VAL_CAUDALHORA=%.0f", caudalHora );
+    uart_write_bytes(UART_Numero, scaudalH, strlen(scaudalH));
+    uart_write_bytes(UART_Numero, "\n", 1); // Agregar salto de línea para separar los comandos
+    
+    vTaskDelay( UART_TiempoEnvioMS / portTICK_PERIOD_MS );
+
+    };
+    
+
+}
+
 void app_main() 
 {
     gpio_reset_pin(PinCaudalimetro);
@@ -176,6 +232,7 @@ void app_main()
 
     InitCaudalimetroISR();
     InitUltrasonico();
+    InitUART(); //inicializo el puerto UART para enviar y recibir datos hacia y desde el ESP32 remoto. 
 
     gpio_set_direction(PinCaudalimetro, GPIO_MODE_INPUT); 
 
@@ -186,6 +243,8 @@ void app_main()
 
     xTaskCreate(CalcularCaudal, "CalcularCaudal", 2048, NULL, 1, NULL);
     xTaskCreate(CalculoDistancia, "TareaUltrasonido", 2048, NULL, 1, NULL);
+    //Creo la tarea que envia los datos a la terminal central con las lecturas de censores. 
+    xTaskCreate(UART_EnviarLecturas, "TareaUART_EnviarLecturas", 2048, NULL, 1, NULL);
 
     /*while (1)
     {
